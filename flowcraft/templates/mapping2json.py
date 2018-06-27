8fec55a8ea2b7878ffc48ff3eda90237b24d0d5c
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-06-27 09:21:11 +0100

    attempted fix in comparison between counter and interval

--- flowcraft/templates/mapping2json.py
+++ flowcraft/templates/mapping2json.py
@@ -108,7 +108,7 @@ def depthfilereader(depth_file, plasmid_length, cutoff):
 
         depth_dic_coverage[reference][position] = numreadsalign
 
-        if interval & counter < interval:
+        if interval & float(counter) < interval:
             counter += 1
             array_of_cov.append(numreadsalign)
         else:
